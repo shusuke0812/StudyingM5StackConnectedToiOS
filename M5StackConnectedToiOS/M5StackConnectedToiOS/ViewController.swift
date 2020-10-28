eaf923f9368ccf7eac0559cf2cc67f6145e39166
shusuke0812 <shu.ota0812@gmail.com>
2020-10-28 15:37:05 +0900

    [add]iOSアプリ開発用のプロジェクトファイルを追加

---
+++ M5StackConnectedToiOS/M5StackConnectedToiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M5StackConnectedToiOS
+//
+//  Created by Shusuke Ota on 2020/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
