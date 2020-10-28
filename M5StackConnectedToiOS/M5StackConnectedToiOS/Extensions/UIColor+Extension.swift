64e643c7291aff14cbfe5d63ed956dc6ccf02495
shusuke0812 <shu.ota0812@gmail.com>
2020-10-28 19:35:06 +0900

    [add]コメント追記

--- M5StackConnectedToiOS/M5StackConnectedToiOS/Extensions/UIColor+Extension.swift
+++ M5StackConnectedToiOS/M5StackConnectedToiOS/Extensions/UIColor+Extension.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UIColor {
+    /// RGB値を指定してUIColorを設定する
     internal func setRGB(r: Int, g: Int, b: Int, alpha: CGFloat) -> UIColor {
         return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
     }
