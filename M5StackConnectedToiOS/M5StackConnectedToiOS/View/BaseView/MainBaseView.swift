24597c065bd544917cf3b8d47f293005ca2cf904
shusuke0812 <shu.ota0812@gmail.com>
2020-10-28 16:53:01 +0900

    [add]Viewファイルを追加

---
+++ M5StackConnectedToiOS/M5StackConnectedToiOS/View/BaseView/MainBaseView.swift
@@ -0,0 +1,20 @@
+//
+//  MainBaseView.swift
+//  M5StackConnectedToiOS
+//
+//  Created by Shusuke Ota on 2020/10/28.
+//
+
+import UIKit
+
+class MainBaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
