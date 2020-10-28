f23fe7656d75410309fdb1eca239cace37117af2
shusuke0812 <shu.ota0812@gmail.com>
2020-10-28 19:28:54 +0900

    [add]UIColorをRGB値で指定できるようなExtensionを追加

---
+++ M5StackConnectedToiOS/M5StackConnectedToiOS/Extensions/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  M5StackConnectedToiOS
+//
+//  Created by Shusuke Ota on 2020/10/28.
+//
+
+import UIKit
+
+extension UIColor {
+    internal func setRGB(r: Int, g: Int, b: Int, alpha: CGFloat) -> UIColor {
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
+    }
+}
